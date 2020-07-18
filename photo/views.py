1a1659e6a01a9e66359d287d4208fc276cef5487
kim-0919 <gustmd5485@gmail.com>
2020-07-18 11:07:42 +0900

    third commit

---
+++ photo/views.py
@@ -0,0 +1,78 @@
+from django.shortcuts import render, redirect
+from django.views.generic.list import ListView
+from django.views.generic.edit import UpdateView, CreateView, DeleteView
+from django.views.generic.detail import DetailView
+from .models import Photo
+from django.contrib import messages 
+from django.http import HttpResponseRedirect, HttpResponseForbidden
+
+
+
+class PhotoList(ListView):
+    model = Photo
+    template_name_suffix = '_list'
+
+class PhotoCreate(CreateView):
+    model = Photo
+    fields = ['text', 'image']
+    template_name_suffix = '_create'
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.instance.author_id = self.request.user.id
+        if form.is_valid():
+            form.instance.save()
+            return redirect('/')
+        else:
+            return self.render_to_response({'form': form})
+
+
+
+class PhotoUpdate(UpdateView):
+    model = Photo
+    fields = ['text', 'image']
+    template_name_suffix = '_update'
+    success_url = '/'
+
+    def dispatch(self, request, *args, **kwargs):
+        object = self.get_object()
+
+        if object.author != request.user:
+            messages.warning(request, '수정할 권한이 없습니다.')
+
+            return HttpResponseRedirect('/')
+
+        else:
+            return super(PhotoUpdate, self).dispatch(request, *args, **kwargs)
+
+class PhotoDelete(DeleteView):
+    model = Photo
+    template_name_suffix = '_delete'
+    success_url = '/'
+
+    def dispatch(self, request, *args, **kwargs):
+        object = self.get_object()
+
+        if object.author != request.user:
+            messages.warning(request, '삭제할 권한이 없습니다.')
+
+            return HttpResponseRedirect('/')
+            
+        return super(PhotoDelete, self).dispatch(request, *args, **kwargs)
+
+class PhotoDetail(DetailView):
+    model = Photo
+    template_name_suffix = '_detail'
+
+class PhotoMyList(ListView):
+    model = Photo
+    template_name = 'photo/photo_mylist.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            messages.warning(request, '먼저 로그인하세요.')
+
+            return HttpResponseRedirect('/')
+            
+        return super(PhotoMyList, self).dispatch(request, *args, **kwargs)
+        
