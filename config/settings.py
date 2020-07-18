8732c8b74dcd2a7f9f3a9625c4a57c34c9d88724
kim-0919 <gustmd5485@gmail.com>
2020-07-18 12:00:00 +0900

    allowed host update

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'm$$$czm-$1)qx#kz)12+pb094m0u$)^ez-+v41#xqg4ce#lkbr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".ap-northeast-2.compute.amazonaws.com"
+]
 
 
 # Application definition
